d643d3984efad1fd4467747796c8650d3ff5f0aa
min4849 <min4849@naver.com>
2022-10-28 15:02:41 +0900

    Feat. Train: 모델 학습 세팅

--- Model/train/kmy/model/mymodel.py
+++ Model/train/kmy/model/mymodel.py
@@ -34,3 +34,27 @@ class MyModel2(tf.keras.Model):
         x = self.headModel3(x)
 
         return self.outputs(x)
+
+
+
+class MyModel3(tf.keras.Model):
+    def __init__(self):
+        super(MyModel3, self).__init__()
+        
+        self.baseModel = tf.keras.applications.efficientnet_v2.EfficientNetV2B3(include_top=False, input_shape=(300, 300, 3))
+        self.norm = tf.keras.layers.BatchNormalization()
+        self.activation = tf.keras.layers.ReLU()
+        self.headModel1 = tf.keras.layers.Dropout(0.5)
+        self.headModel2 = tf.keras.layers.GlobalAveragePooling2D()
+        self.headModel3 = tf.keras.layers.Dropout(0.5)
+        self.outputs = tf.keras.layers.Dense(2, activation="softmax")
+    
+    def call(self, x):
+        x = self.baseModel(x)
+        x = self.norm(x)
+        x = self.activation(x)
+        x = self.headModel1(x)
+        x = self.headModel2(x)
+        x = self.headModel3(x)
+        
+        return self.outputs(x)
