5043a6204ef6ac2f90096f7919b09b981079374d
홍성덕 <tjdejr338@naver.com>
2022-10-29 21:25:11 +0900

    Feat. Train: Resnet50-train-case5,case6

--- Model/train/hsd/model/mymodel.py
+++ Model/train/hsd/model/mymodel.py
@@ -67,3 +67,47 @@ class MyModel4(tf.keras.Model):
         x = self.dense_2(x)
 
         return self.d2(x)
+
+class MyModel5(tf.keras.Model):
+    def __init__(self):
+        super(MyModel5, self).__init__()
+        
+        self.baseModel = tf.keras.applications.resnet50.ResNet50(include_top=False, input_shape=(300, 300, 3))
+        self.norm = tf.keras.layers.BatchNormalization()
+        self.activation = tf.keras.layers.ReLU()
+        self.headModel1 = tf.keras.layers.Dropout(0.5)
+        self.headModel2 = tf.keras.layers.GlobalAveragePooling2D()
+        self.headModel3 = tf.keras.layers.Dropout(0.5)
+        self.outputs = tf.keras.layers.Dense(2, activation="softmax")
+    
+    def call(self, x):
+        x = self.baseModel(x)
+        x = self.norm(x)
+        x = self.activation(x)
+        x = self.headModel1(x)
+        x = self.headModel2(x)
+        x = self.headModel3(x)
+        
+        return self.outputs(x)
+
+class MyModel6(tf.keras.Model):
+    def __init__(self):
+        super(MyModel6, self).__init__()
+        
+        self.baseModel = tf.keras.applications.resnet50.ResNet50(include_top=False, input_shape=(300, 300, 3))
+        self.norm = tf.keras.layers.BatchNormalization()
+        self.activation = tf.keras.layers.ReLU()
+        self.headModel1 = tf.keras.layers.Dropout(0.5)
+        self.headModel2 = tf.keras.layers.GlobalAveragePooling2D()
+        self.headModel3 = tf.keras.layers.Dropout(0.5)
+        self.outputs = tf.keras.layers.Dense(2, activation="softmax")
+    
+    def call(self, x):
+        x = self.baseModel(x)
+        x = self.norm(x)
+        x = self.activation(x)
+        x = self.headModel1(x)
+        x = self.headModel2(x)
+        x = self.headModel3(x)
+        
+        return self.outputs(x)
