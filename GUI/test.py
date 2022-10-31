56df4b6964eef32d6fbaf1a3f018dab83ff197af
min4849 <min4849@naver.com>
2022-10-31 15:00:32 +0900

    Feat. GUI: training init 파일 위치 열기 모달

--- GUI/test.py
+++ GUI/test.py
@@ -6,24 +6,43 @@
 # print(dic)
 
 
-from numpy import array
+# from numpy import array
 
 
-class hyperparameter:
-    def __init__(self):
-        self.parameterName = ""
-        self.parameterValue = 0
-    def changeValue(self, newValue):
-        self.parameterValue = newValue
+# class hyperparameter:
+#     def __init__(self):
+#         self.parameterName = ""
+#         self.parameterValue = 0
+#     def changeValue(self, newValue):
+#         self.parameterValue = newValue
 
-class main:
-    # def __init__(self):
-    def changeParma(self, value):
-        return 0
+# class main:
+#     # def __init__(self):
+#     def changeParma(self, value):
+#         return 0
 
-a = {}
+# a = {}
 
-a["now"] = "happy"
-a["def"] = "not"
-a[1] = "1234"
-print(a)
+# a["now"] = "happy"
+# a["def"] = "not"
+# a[1] = "1234"
+# print(a)
+
+
+
+
+# def decorate(func):
+#     def wrapper():
+#         print("wrapping")
+#         func()
+#     return wrapper
+
+# @decorate
+# def hello():
+#     print("hello")
+
+# hello()
+
+
+a = "(123)"
+print(a[1:-2])
