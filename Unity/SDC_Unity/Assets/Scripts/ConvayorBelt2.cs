b0e61bd52a8c5339d60bf9e159c2a1bfd7a7c4d6
성덕 <97648090+HSungDuk@users.noreply.github.com>
2022-11-02 17:25:57 +0900

    Feat. Unity: 이미지 스크린샷 기능 추가
    서버 구축 시 전송할 이미지 데이터 생성

--- Unity/SDC_Unity/Assets/Scripts/ConvayorBelt2.cs
+++ Unity/SDC_Unity/Assets/Scripts/ConvayorBelt2.cs
@@ -26,16 +26,38 @@ public class ConvayorBelt2 : MonoBehaviour
 
         if (fTime >= fDelayTime)
         {
+            
             Vector3 pos = rBody.position;
             rBody.position += Vector3.right * speed * Time.fixedDeltaTime;
             rBody.MovePosition(pos);
-
+            
             if (fTime >= 6)
             {
                 Instantiate(Product);
-
                 fTime = 0.00f;
             }
+
+            if (fTime < 2.03)
+            {
+                StartCoroutine ("Rendering");
+            }
         }
+    }
+
+    IEnumerator Rendering() 
+    {
+        yield return new WaitForEndOfFrame ();
+
+        byte[] imgBytes;
+        string path = @"C:\test.png";
+
+        Texture2D texture = new Texture2D (Screen.width/10 * 2, Screen.height /10 * 4, TextureFormat.RGB24, false);
+        texture.ReadPixels (new Rect (0, 0, Screen.width, Screen.height), 0, 0, false);
+        texture.Apply ();
+
+        imgBytes = texture.EncodeToJPG ();
+
+        System.IO.File.WriteAllBytes (path, imgBytes);
+        Debug.Log(path + "has been saved");
     }
 }
--- Unity/SDC_Unity/Assets/Scripts/ScreenShot.cs
+++ Unity/SDC_Unity/Assets/Scripts/ScreenShot.cs
@@ -1,41 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
- 
-public class ScreenShot : MonoBehaviour {
-    public Camera camera;       //보여지는 카메라.
- 
-    private int resWidth;
-    private int resHeight;
-    string path;
-    // Use this for initialization
-    void Start () {
-        resWidth = Screen.width;
-        resHeight = Screen.height;
-        path = Application.dataPath+"/ScreenShot/";
-        Debug.Log(path);
-    }
- 
-    public void ClickScreenShot()
+
+public class ScreenShot : MonoBehaviour
+{
+    private void Awake()
     {
-        DirectoryInfo dir = new DirectoryInfo(path);
-        if (!dir.Exists)
-        {
-            Directory.CreateDirectory(path);
-        }
-        string name;
-        name = path + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
-        RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
-        camera.targetTexture = rt;
-        Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
-        Rect rec = new Rect(0, 0, screenShot.width, screenShot.height);
-        camera.Render();
-        RenderTexture.active = rt;
-        screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
-        screenShot.Apply();
- 
-        byte[] bytes = screenShot.EncodeToPNG();
-        File.WriteAllBytes(name, bytes);
+        StartCoroutine ("Rendering");
+    }
+
+    IEnumerator Rendering() 
+    {
+        yield return new WaitForEndOfFrame ();
+
+        byte[] imgBytes;
+        string path = @"C:\test.png";
+
+        Texture2D texture = new Texture2D (Screen.width/10 * 2, Screen.height /10 * 4, TextureFormat.RGB24, false);
+        texture.ReadPixels (new Rect (0, 0, Screen.width, Screen.height), 0, 0, false);
+        texture.Apply ();
+
+        imgBytes = texture.EncodeToJPG ();
+        System.IO.File.WriteAllBytes (path, imgBytes);
+        Debug.Log(path + "has been saved");
     }
 }
