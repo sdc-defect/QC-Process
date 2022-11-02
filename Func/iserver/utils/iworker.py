26a58da2b8c821b2c4651db44bf040c12dc47fc3
jhno96 <53774255+SnowFlower96@users.noreply.github.com>
2022-11-02 17:48:55 +0900

    Refact. Func: 소켓 관련 삭제

--- Func/iserver/utils/iworker.py
+++ Func/iserver/utils/iworker.py
@@ -58,12 +58,3 @@ class IWorker:
             cam = cv2.cvtColor(cam, cv2.COLOR_BGR2RGB)
 
         return cam
-
-
-if __name__ == "__main__":
-    wo = IWorker(onnx_path="../temp/case2/onnx/modified.onnx", npy_path="../temp/case2/onnx/dense.npy")
-    im = cv2.imread("../temp/dataset/test/1_def/def_7724.jpeg")
-    result = wo.inference(im)
-
-    cv2.imshow("concat", cv2.hconcat((result.cam / 255, result.merged)))
-    cv2.waitKey()
