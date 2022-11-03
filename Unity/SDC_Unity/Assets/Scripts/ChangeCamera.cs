0358a4504ff63dcb0f6bfbd450dbf32b32022ac2
성덕 <97648090+HSungDuk@users.noreply.github.com>
2022-11-03 13:56:40 +0900

    Feat. Unity: dataset 입히기

--- Unity/SDC_Unity/Assets/Scripts/ChangeCamera.cs
+++ Unity/SDC_Unity/Assets/Scripts/ChangeCamera.cs
@@ -4,23 +4,14 @@ using UnityEngine;
 
 public class ChangeCamera : MonoBehaviour
 {
-	public Camera[] arrCam; //카메라 요소들을 추가한다.
-	int nCamCount = 2;
-	int nNowCam = 0;
+	public Camera MiniCam;
 
 	void Update ()
 	{
 		if( Input.GetButtonUp("Fire3"))
 		{
-			++nNowCam;
-			if (nNowCam >= nCamCount)
-			{
-				nNowCam = 0;
-			}
-			for ( int i=0; i<arrCam.Length; ++i )
-			{
-				arrCam[i].enabled = (i == nNowCam);
-			}
+			MiniCam.enabled = !MiniCam.enabled;
+
 		}
 	}
 }
--- Unity/SDC_Unity/Assets/Scripts/ConvayorBelt2.cs
+++ Unity/SDC_Unity/Assets/Scripts/ConvayorBelt2.cs
@@ -9,6 +9,10 @@ public class ConvayorBelt2 : MonoBehaviour
     private float fTime = 0.00f;
     public GameObject Product;
     Rigidbody rBody;
+    public Material[] mat = new Material[13];
+    bool isCapture = true;
+    bool isFirstCapture = true;
+    int i = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -33,12 +37,17 @@ public class ConvayorBelt2 : MonoBehaviour
             
             if (fTime >= 6)
             {
-                Instantiate(Product);
+                i = ++i % 13;
+                
+                GameObject create = Instantiate(Product);
+                create.gameObject.GetComponent<MeshRenderer>().material = mat[i];
                 fTime = 0.00f;
+                isCapture = true;
             }
-
-            if (fTime < 2.03)
+            // print(fTime);
+            if (fTime != 0 && isCapture)
             {
+                isCapture = false;
                 StartCoroutine ("Rendering");
             }
         }
@@ -46,18 +55,26 @@ public class ConvayorBelt2 : MonoBehaviour
 
     IEnumerator Rendering() 
     {
-        yield return new WaitForEndOfFrame ();
+        if (isFirstCapture)    
+        {
+            isFirstCapture = false;
+            
+        }
+        else
+        {
+            yield return new WaitForEndOfFrame ();
 
-        byte[] imgBytes;
-        string path = @"C:\test.png";
+            byte[] imgBytes;
+            string path = string.Format(@"C:\ok{0}.png", i);
+            Texture2D texture = new Texture2D (Screen.width/10 * 2, Screen.height /10 * 4, TextureFormat.RGB24, false);
+            texture.ReadPixels (new Rect (0, 0, Screen.width, Screen.height), 0, 0, false);
+            texture.Apply ();
 
-        Texture2D texture = new Texture2D (Screen.width/10 * 2, Screen.height /10 * 4, TextureFormat.RGB24, false);
-        texture.ReadPixels (new Rect (0, 0, Screen.width, Screen.height), 0, 0, false);
-        texture.Apply ();
+            imgBytes = texture.EncodeToJPG ();
 
-        imgBytes = texture.EncodeToJPG ();
+            System.IO.File.WriteAllBytes (path, imgBytes);
+            // Debug.Log(path + "has been saved");
 
-        System.IO.File.WriteAllBytes (path, imgBytes);
-        Debug.Log(path + "has been saved");
+        }
     }
 }
