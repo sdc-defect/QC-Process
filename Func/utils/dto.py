efeaa43f393674ff1b8d1813ca7962ebd59c1a42
고승효 <shko0524@naver.com>
2022-11-03 17:35:10 +0900

    Revert "Merge branch 'feat-ksh/websocket-server' into 'develop'"
    This reverts merge request !65

--- Func/utils/dto.py
+++ Func/utils/dto.py
@@ -1,8 +1,7 @@
-from typing import List, Union, Tuple
+from typing import List
 
 from dataclasses import dataclass
 import numpy as np
-import onnxruntime
 
 
 @dataclass
@@ -11,10 +10,3 @@ class InferenceResult:
     prob: List[float]
     cam: np.ndarray
     merged: np.ndarray
-
-
-@dataclass
-class ONNXRuntime:
-    runtime: onnxruntime.InferenceSession
-    dense: np.ndarray
-
