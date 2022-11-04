6cd7a958c3ce102fbf362a849a9598b98646e004
고승효 <shko0524@naver.com>
2022-11-04 10:18:42 +0900

    feat-ksh/websocket-server 연결

--- Func/main.py
+++ Func/main.py
@@ -1,8 +1,10 @@
 from enum import Enum
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, status, WebSocket, Request
+
+from fastapi.responses import HTMLResponse
+from fastapi.logger import logger
 
 app = FastAPI()
-
 
 # class ModelName(str, Enum):
 #     alexnet = "alexnet"
@@ -13,6 +15,18 @@ app = FastAPI()
 @app.post("/conn", status_code=200)
 async def connection():
     return { "messege" : "Successfully Connected"}
+
+# 웹소켓 설정 ws://127.0.0.1:8000/ws 로 접속할 수 있음
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    print(f"client connected : {websocket.client}")
+    await websocket.accept() # client의 websocket접속 허용
+    await websocket.send_text(f"Welcome client : {websocket.client}")
+    while True:
+        data = await websocket.receive_text()  # client 메시지 수신대기
+        print(f"message received : {data} from : {websocket.client}")
+        await websocket.send_text(f"Message text was: {data}") # client에 메시지 전달
+
 
 @app.post("/start", status_code=200)
 async def start_isystem():
@@ -35,7 +49,14 @@ async def pause_isystem():
     return { "messege" : "Successfully Paused"}
     
 
+# 개발/디버깅용으로 사용할 앱 구동 함수
+def run():
+    import uvicorn
+    uvicorn.run(app)
 
+
+if __name__ == "__main__":
+    run()
 # ===========================================================================================================
 # ===========================================================================================================
 
