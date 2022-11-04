4a0f4767d81e01f19b27ad3360172f1eda847c12
고승효 <shko0524@naver.com>
2022-11-04 13:15:10 +0900

    Feat. Func: websocket send image

--- Func/utils/data.py
+++ Func/utils/data.py
@@ -11,9 +11,7 @@ def transfer_image(result: InferenceResult):
 
 
 def image_to_base64(img: np.ndarray) -> bytes:
-    """ Given a numpy 2D array, returns a JPEG image in base64 format """
-
-    # using opencv 2, there are others ways
+    
     img_buffer = cv2.imencode('.jpg', img)[1]
     
     return base64.b64encode(img_buffer).decode('utf-8')
