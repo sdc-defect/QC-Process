853c7664bc7a8ad639fff07a9a53e79e89a1e4e0
jhno96 <53774255+SnowFlower96@users.noreply.github.com>
2022-11-04 17:56:22 +0900

    Feat. Func: 서버 파일 통합

--- Func/main.py
+++ Func/main.py
@@ -1,70 +1,75 @@
-from enum import Enum
-from utils.data import Large_Data
+import time
 
-from fastapi import FastAPI, Response, status, WebSocket, Request
-
-from fastapi.responses import HTMLResponse
-from fastapi.logger import logger
-
+from fastapi import FastAPI, Response, WebSocket
 from starlette.websockets import WebSocketDisconnect
+
+from utils.data import transfer_image
+from utils.iworker import IService
+
 
 app = FastAPI()
 service = IService()
 is_connected = True
 
+
 @app.post("/conn", status_code=200)
 async def connection():
-    return { "messege" : "Successfully Connected"}
+    return {"message": "Successfully Connected"}
+
 
 # 웹소켓 설정
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     print(f"client connected : {websocket.client}")
-    await websocket.accept() # client의 websocket접속 허용
+    await websocket.accept()
     try:
         await websocket.send_text(f"Welcome client : {websocket.client}")
         while True:
-            if service :
-                pass
-            # 수정해야됨 메시지 -> 이미지
-            data = await websocket.receive_text()  # client 메시지 수신대기
-            print(f"message received : {data} from : {websocket.client}")
-            await websocket.send_text(f"Message text was: {data}") # client에 메시지 전달
+            time.sleep(0.1)
+            if not service.queue.empty():
+                result = service.queue.get()
+                data = transfer_image(result)
+                await websocket.send_text(f"Message text was: {data}")
+
     except WebSocketDisconnect:
         await websocket.close()
+
 
 @app.post("/conn", status_code=200)
 async def connection():
     if is_connected:
-        return { "messege" : "Successfully Connected"}
+        return {"message": "Successfully Connected"}
     else:
-        return { "messege" : "Connection Failed"}
-    
+        return {"message": "Connection Failed"}
+
+
 @app.post("/start", status_code=200)
 async def start_isystem(response: Response):
     check = service.check_process_alive()
     if check:
         response.status_code = 409
-        return { "messege" : "Already Started"}
+        return {"message": "Already Started"}
     else:
         service.update_onnx_info("./data/modified.onnx")
         service.run_process()
-        return { "messege" : "Successfully Started"}
+        return {"message": "Successfully Started"}
+
 
 @app.post("/stop", status_code=200)
 async def stop_isystem(response: Response):
     check = service.check_process_alive()
     if not check:
         response.status_code = 409
-        return { "messege" : "Already Stopped"}
+        return {"message": "Already Stopped"}
     else:
         await service.stop_inference()
-        return { "messege" : "Successfully Stopped"}
+        return {"message": "Successfully Stopped"}
+
 
 @app.post("/pause", status_code=200)
 async def pause_isystem():
-    return { "messege" : "Successfully Paused"}
-    
+    return {"message": "Successfully Paused"}
+
 
 # 개발/디버깅용으로 사용할 앱 구동 함수
 def run():
