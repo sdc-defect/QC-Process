d5a79a63b0b866eec1b0f30913a0405097a3b1d8
홍성덕 <tjdejr338@naver.com>
2022-11-08 20:46:24 +0900

    Feat. Train: Resnet50-model15

--- Model/train/hsd/model/mymodel.py
+++ Model/train/hsd/model/mymodel.py
@@ -65,6 +65,29 @@ class MyModel14(tf.keras.Model):
         
         return self.outputs(x)
 
+class MyModel15(tf.keras.Model):
+    def __init__(self):
+        super(MyModel15, self).__init__()
+        
+        self.baseModel = tf.keras.applications.resnet50.ResNet50(include_top=False, input_shape=(300, 300, 3))
+        self.norm = tf.keras.layers.BatchNormalization()
+        self.activation = tf.keras.layers.ReLU()
+        #  headModel1, headModeal3 Dropout을 0.3 -> 0.7 로 0.1씩 증가
+        self.headModel1 = tf.keras.layers.Dropout(0.4)
+        self.headModel2 = tf.keras.layers.GlobalAveragePooling2D()
+        self.headModel3 = tf.keras.layers.Dropout(0.4)
+        self.outputs = tf.keras.layers.Dense(2, activation="softmax")
+    
+    def call(self, x):
+        x = self.baseModel(x)
+        x = self.norm(x)
+        x = self.activation(x)
+        x = self.headModel1(x)
+        x = self.headModel2(x)
+        x = self.headModel3(x)
+        
+        return self.outputs(x)
+
 test = MyModel2()
 test.build(input_shape=(None, 300, 300, 3))
 test.summary()
