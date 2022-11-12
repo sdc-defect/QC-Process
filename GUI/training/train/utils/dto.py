9c7c32c0186402fcc3dafaa6fd778d55b5399d51
DESKTOP-LOGMHEP\jhno9 <jhno96@gmail.com>
2022-11-12 18:01:25 +0900

    Feat. GUI: 학습 프로세스 CLI화

---
+++ GUI/training/train/utils/dto.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+from typing import Union, List
+
+import onnxruntime
+import numpy as np
+
+
+@dataclass
+class ConfusionMatrix:
+    tp: int
+    tn: int
+    fp: int
+    fn: int
+
+
+@dataclass
+class ONNXRuntime:
+    runtime: onnxruntime.InferenceSession
+    dense: np.ndarray
+
+
+@dataclass
+class TrainConfig:
+    save_path: str
+    train_path: List[str]
+
+    test_path: Union[List[str], None]
+    test_per: Union[float, None]
+
+    val_path: Union[List[str], None]
+    val_per: Union[float, None]
+
+    flip: bool = False
+    spin: bool = False
+    shift: bool = False
+    mixup: bool = False
+
+    epoch: int = 50
+    batch_size: int = 16
+    lr: float = 0.001
+    decay: int = 1000
+
+
+@dataclass
+class TrainResult:
+    confusionmatrix: ConfusionMatrix
+    loss: float = 0.523234
+    header: str = "train/val/test"
+    epoch: Union[str, None] = "1/50"
+    batch: str = "3/20"
